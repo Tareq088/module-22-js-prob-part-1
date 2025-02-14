51819afd90e51b482d2dc03ff51e51732bf23f8b
Tareq1203088 <trahmanbpdb19@gmail.com>
2025-02-14 12:37:15 +0600

    first commit

---
+++ practice_task.js
@@ -0,0 +1,11 @@
+
+                // ### Task-1: 
+// Write a function to convert temperature from Celsius to Fahrenheit.
+
+function celToFaren(){
+    
+}
+let celsius =  45;
+console.log(celToFaren(celsius))
+
+// ---
